3cb27f4a9bc04b31508f0060c13d9068ce4139e6
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-07-09 16:31:07 -0600

    Created React app, added navbar

---
+++ src/navigation/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+
+  render(){
+    return (
+            <nav>
+
+            </nav>
+    )
+  }
+}
+
+export default NavBar
